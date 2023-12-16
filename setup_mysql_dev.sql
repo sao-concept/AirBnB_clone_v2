aa1302794fa152e025e07efaf769a721617c4a04
Azeez Saka <saoconcept1@gmail.com>
2023-12-16 09:23:16 +0100

    add setup_mysql_dev.sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -11,7 +11,7 @@ IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.*
 TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT privilege on performance_schema to the user hbnb_dev
+-- Grant SELECT privileges on performance_schema to the user hbnb_dev
 GRANT SELECT ON performance_schema.*
 TO 'hbnb_dev'@'localhost';
 
