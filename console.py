e9ae227353ea3436abc5017c6f71d543f9d2c07b
Azeez Saka <saoconcept1@gmail.com>
2023-12-16 09:16:40 +0100

    update console.py, tests/test_models/test_base_model.py scripts

--- console.py
+++ console.py
@@ -177,7 +177,7 @@ class HBNBCommand(cmd.Cmd):
         key = f"{c_name}.{c_id}"
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -296,4 +296,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
