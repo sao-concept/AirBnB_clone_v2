1f1d26034f28d12f05d06e32b17ef4693d433d76
Azeez Saka <saoconcept1@gmail.com>
2023-12-17 22:44:07 +0100

    update models/state.py script

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from os import getenv
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
 
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Getter attribute in case of file storage"""
+            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
