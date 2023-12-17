1c96547aa378dadce5302bd501d7464cd0f7a436
Azeez Saka <saoconcept1@gmail.com>
2023-12-17 22:28:32 +0100

    modify setup_mysql_test.sql file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,91 +1,14 @@
--- Test Case 1: Basic Test - Clean Slate
--- Expect Outcome: Create the database, user, and grant privileges
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Step 1: Create a test database for the Airbnb clone
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Test Case 2: Database and User Exist
--- Expected Outcome: No changes should be made
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Step 2: Create a user and grant privileges for the test database
+-- Note: Replace 'hbnb_test' and 'hbnb_test_pwd' with your desired username and password
+GRANT ALL ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
 
--- Test Case 3: Database Exists, User Does Not Exist
--- Expected Outcome: Create the user and grant privileges
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Step 3: Grant SELECT privileges on the performance_schema for the test user
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
 
--- Test Case 4: Database Does Not Exist, User Exists
--- Expected Outcome: Create the database and grant privileges
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
--- Test Case 5: Incorrect Password
--- Expected Outcome: Error or warning related to authentication
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'incorrect_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
--- Test Case 6: Invalid Host
--- Expected Outcome: Appropriate error or warning message
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'invalid_host' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'invalid_host';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'invalid_host';
-FLUSH PRIVILEGES;
-
--- Test Case 7: Missing Database Name
--- Expected Outcome: Error or warning indicating that the database name is required
--- (Note: Adjust the query to intentionally miss the database name)
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS '@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
--- Test Case 8: Missing User Name
--- Expected Outcome: Error or warning indicating that the user name is required
--- (Note: Adjust the query to intentionally miss the user name)
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS '@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO '@'localhost';
-GRANT SELECT ON performance_schema.* TO '@'localhost';
-FLUSH PRIVILEGES;
-
--- Test Case 9: Revoke Privileges
--- Expected Outcome: Revoke privileges as specified
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-REVOKE SELECT ON performance_schema.* FROM 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
--- Test Case 10: Performance Schema Access
--- Expected Outcome: Grant SELECT privileges on the performance_schema database
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
--- Test Case 11: Flush Privileges
--- Expected Outcome: Apply changes immediately after flushing privileges
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
+-- Note: Adjust the privileges and database/user details as needed for your specific requirements.
