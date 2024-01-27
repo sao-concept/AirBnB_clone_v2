afdf02f66a9ad65bbe24fc8a9db745fcc978fd19
Azeez Saka <saoconcept1@gmail.com>
2024-01-27 14:22:36 -0800

    add 4-number_route.py script

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display_hello():
+    """This function prints hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def desplay_hbnb():
+    """This function print hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_cText(text):
+    """This function print C with passed variable"""
+    text = text.replace("_", " ")
+    return "C %s" % (text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_pythonText(text='is cool'):
+    """ This function is called with /python/<text> route """
+    if text != 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % (text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_if_int(n):
+    return '%d is a number' % (n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
