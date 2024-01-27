6c6ce8ab0aa09078e2078a1cfc7f5a61f50065b1
Azeez Saka <saoconcept1@gmail.com>
2024-01-27 05:12:10 -0800

    modify 1-hbnb_route.py script

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-""" Fabfile that create a .tgz archive from
-the contents of web_static folder"""
+"""A script that starts a Flask web application"""
+from flask import Flask
 
-# if __name__ == '__main__':
-from fabric.api import local
-from datetime import datetime
+app = Flask(__name__)
 
 
-def do_pack():
-    """Pack all the contents in the web_static directory as a tar archive"""
+@app.route("/", strict_slashes=False)
+def display_hello():
+    return "Hello HBNB!"
 
-    try:
-        local("mkdir -p versions")
-        time = datetime.now()
-        date_string = '%Y%m%d%H%M%S'
-        date = time.strftime(date_string)
 
-        file_path = "versions/web_static_{}.tgz".format(date)
-        local("tar -czvf {} web_static".format(file_path))
-        return file_path
+@app.route("/hbnb", strict_slashes=False)
+def desplay_hbnb():
+    return "HBNB"
 
-    except Exception:
-        return None
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
